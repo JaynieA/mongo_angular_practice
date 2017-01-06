34f9785b6dd54e4d05a1ca8cde8323b9c364633f
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-05 19:05:38 -0600

    sourced angular

---
+++ public/script/client.js
@@ -0,0 +1,9 @@
+console.log('sourcing client.js');
+//hook up NG
+var app = angular.module('app', []);
+
+//create controller
+app.controller('AssignmentController', ['$scope', function($scope) {
+  console.log('NG');
+  
+}]); // end controller
